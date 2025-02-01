75a849dcd53a355ca997e0f86d59a451508e1a0f
kimyongjun0129 <kimyongjun0129@naver.com>
2025-02-01 15:26:28 +0900

    add mixins folder
    1. add test object
    2. add mixins folder to reduce code iteration for test-object

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,25 @@
-export const trending = (req, res) => res.render("home", {pageTitle: "Home"});
+export const trending = (req, res) => 
+{
+    const videos = [
+        {
+            title: "First Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "1 minutes age",
+            views: 59,
+            id: 1,
+        },
+        {
+            title: "Second Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes age",
+            views: 76,
+            id: 2,
+        },
+    ]
+    return res.render("home", {pageTitle: "Home", videos: videos});
+}
 export const see = (req, res) => res.render("watch", {pageTitle: "Watch"});
 export const edit = (req, res) => res.render("edit", {pageTitle: "Edit"});
 export const remove = (req, res) => res.send("Remove Video");
