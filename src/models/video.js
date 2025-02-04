37bc1032e7f7113085faf08c2aa653544f94a837
kimyongjun0129 <kimyongjun0129@naver.com>
2025-02-04 23:12:37 +0900

    add data quality
    Add detailed information to improve data quality

--- src/models/video.js
+++ src/models/video.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    createdAt: Date,
-    hashtags: [{type:String}],
-    meta: {
-        views: Number,
-        rating: Number,
+    title: {type: String, required: true, trim: true, maxLength: 80},
+    description: {type: String, required: true, trim: true, maxLength: 20},
+    createdAt: {type: Date, required: true, default: Date.now},
+    hashtags: [{type:String, trim: true}],
+    meta: { 
+        views: { type: Number, default: 0, required: true},
+        rating: { type: Number, default: 0, required: true},
     },
 });
 
