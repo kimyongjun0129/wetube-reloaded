2b8a3803f6afd75c409988c49dce3361943c11d1
kimyongjun0129 <kimyongjun0129@naver.com>
2025-02-04 23:07:40 +0900

    can be stored video data in the DB
    upgrade postEdit controller so that video data can be stored in the DB

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -11,27 +11,51 @@ export const home = async(req, res) =>
     {
         return res.render("server-error", {error});
     }
-}
-export const watch = (req, res) => 
+};
+
+export const watch = async (req, res) => 
 {
     const { id } = req.params;  // const id = req.params.id;
-    return res.render("watch", {pageTitle: `Watching`});
+    const video = await videoModel.findById(id);
+    if(!video) {
+        return res.render("404", {pageTitle:"Video not found."});
+    }
+    return res.render("watch", {pageTitle: video.title, video});
 };
-export const getEdit = (req, res) => 
+
+export const getEdit = async (req, res) => 
 {
     const { id } = req.params;
-    return res.render("edit", {pageTitle: `Editing`});
+    const video = await videoModel.findById(id);
+    if(!video) {
+        return res.render("404", {pageTitle: "Video not found."});
+    }
+    return res.render("edit", {pageTitle: `Edit: ${video.title}`, video});
 };
-export const postEdit = (req, res) => 
+
+export const postEdit = async (req, res) => 
 {
     const { id } = req.params;
-    const { title } = req.body;
+    const {title, description, hashtags } = req.body;
+    const video = await videoModel.exists({_id:id});
+    if (!video) {
+        return res.render("404", {pageTitle: "Video not found."});
+    }
+    await videoModel.findByIdAndUpdate(id, {
+        title,
+        description,
+        hashtags: hashtags
+        .split(",")
+        .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+    });
     return res.redirect(`/videos/${id}`);
 };
+
 export const getUpload = (req, res) => 
 {
     return res.render("upload", {pageTitle: "Upload Video"});
 }
+
 export const postUpload = async (req, res) =>
 {
     const { title, description, hashtags } = req.body;
@@ -40,7 +64,7 @@ export const postUpload = async (req, res) =>
         await videoModel.create ({
             title, // title:title,
             description, // description:description,
-            hashtags: hashtags.split(",").map(word => `#${word}`),
+            hashtags: hashtags.split(",").map((word)=> (word.startsWith("#") ? word : `#${word}`)),
         });
         return res.redirect("/");
     }
