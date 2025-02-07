7cc581e86fbb610ae4bb50e320b045fc8342c454
kimyongjun0129 <kimyongjun0129@naver.com>
2025-02-07 21:30:14 +0900

    add video router

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getEdit, watch, postEdit, getUpload, postUpload } from "../controllers/videoController.js";
+import { getEdit, watch, postEdit, getUpload, postUpload, deleteVideo } from "../controllers/videoController.js";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id([0-9a-f]{24})", watch);
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
