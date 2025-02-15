ec4171f370c82d4d29652ae8c301d883b5d4f201
kimyongjun0129 <kimyongjun0129@naver.com>
2025-02-15 22:42:54 +0900

    add session middleware

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import globalRouter from "./routers/rootRouter.js";
 import usersRouter from "./routers/userRouter.js";
 import videoRouter from "./routers/videoRouter.js";
+import { localsMiddleware } from "./middlewares.js";
 
 
 const app = express();
@@ -12,6 +14,16 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({extended:true}));
+
+app.use(
+    session({
+    secret: "Hello",
+    resave: true,
+    saveUninitialized: true,
+    })
+);
+
+app.use(localsMiddleware);
 app.use("/videos", videoRouter);
 app.use("/users", usersRouter);
 app.use("/", globalRouter);
