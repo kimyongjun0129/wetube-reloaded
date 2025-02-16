4c0e742dbf864ef7cefbcc371a20d779b1b0ef86
kimyongjun0129 <kimyongjun0129@naver.com>
2025-02-16 15:14:32 +0900

    update session middleware setting
    Always Save Sessions ->Save sessions only when logged in

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import globalRouter from "./routers/rootRouter.js";
 import usersRouter from "./routers/userRouter.js";
 import videoRouter from "./routers/videoRouter.js";
@@ -18,8 +19,9 @@ app.use(express.urlencoded({extended:true}));
 app.use(
     session({
     secret: "Hello",
-    resave: true,
-    saveUninitialized: true,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({mongoUrl: "mongodb://127.0.0.1:27017/wetube"})
     })
 );
 
