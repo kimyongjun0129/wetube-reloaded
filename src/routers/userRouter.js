aac79f0c650f38737c456001a7f7b9f53b28d91f
kimyongjun0129 <kimyongjun0129@naver.com>
2025-02-19 00:53:56 +0900

    add uploadFile middleware

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { getEdit, postEdit, startGithubLogin, finishGithubLogin } from "../controllers/userController.js";
 import { see, logout  } from "../controllers/userController.js";
-import { protectorMiddleware, publicOnlyMiddleware } from "../middlewares.js"
+import { protectorMiddleware, publicOnlyMiddleware, uploadFiles } from "../middlewares.js"
 
 const usersRouter = express.Router();
 
@@ -9,7 +9,7 @@ usersRouter.get("/logout", protectorMiddleware, logout);
 usersRouter.route("/edit")
 .all(protectorMiddleware)
 .get(getEdit)
-.post(postEdit)
+.post(uploadFiles.single("avatar"), postEdit)
 usersRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
 usersRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
 usersRouter.get(":id", see);
