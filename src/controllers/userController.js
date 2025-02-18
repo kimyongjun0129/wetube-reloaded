c8ef22301a223aec62faa5377e502771a429955c
kimyongjun0129 <kimyongjun0129@naver.com>
2025-02-19 00:54:52 +0900

    create getEdit and postEdit

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -141,5 +141,30 @@ export const logout = async (req, res) => {
         return res.redirect("/");
     });
 }
-export const edit = (req, res) => res.send("Edit");
+
+export const getEdit = (req, res) => {
+    return res.render("edit-profile", { pageTitle: "Edit Profile" });
+};
+export const postEdit = async (req, res) => {
+    const {
+      session: {
+        user: { _id },
+      },
+      body: { name, email, username, location },
+      file,
+    } = req;
+    const updatedUser = await User.findByIdAndUpdate(
+      _id,
+      {
+        name,
+        email,
+        username,
+        location,
+      },
+      { new: true }
+    );
+    req.session.user = updatedUser;
+    return res.redirect("/users/edit");
+};
+
 export const see = (req, res) => res.send("See profile");
