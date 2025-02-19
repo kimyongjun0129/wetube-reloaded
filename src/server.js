a11d7e9b482a8f8c530a6e4d1361c736961093f7
kimyongjun0129 <kimyongjun0129@naver.com>
2025-02-20 01:04:35 +0900

    add upload router
    Make upload files accessible to everyone, including browsers

--- src/server.js
+++ src/server.js
@@ -25,6 +25,7 @@ app.use(
 );
 
 app.use(localsMiddleware);
+app.use("/uploads", express.static("uploads"));
 app.use("/videos", videoRouter);
 app.use("/users", usersRouter);
 app.use("/", globalRouter);
