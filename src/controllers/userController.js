dcc0a8ddba37c4755907b5b9d7b6bc6bce341e11
kimyongjun0129 <kimyongjun0129@naver.com>
2025-02-20 23:46:32 +0900

    add change password controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -169,4 +169,47 @@ export const postEdit = async (req, res) => {
     return res.redirect("/users/edit");
 };
 
-export const see = (req, res) => res.send("See profile");
+export const getChangePassword = (req, res) => {
+    if (req.session.user.socialOnly === true) {
+      return res.redirect("/");
+    }
+    return res.render("users/change-password", { pageTitle: "Change Password" });
+  };
+  export const postChangePassword = async (req, res) => {
+    const {
+      session: {
+        user: { _id },
+      },
+      body: { oldPassword, newPassword, newPasswordConfirmation },
+    } = req;
+    const user = await userModel.findById(_id);
+    const ok = await bcrypt.compare(oldPassword, user.password);
+    if (!ok) {
+      return res.status(400).render("users/change-password", {
+        pageTitle: "Change Password",
+        errorMessage: "The current password is incorrect",
+      });
+    }
+    if (newPassword !== newPasswordConfirmation) {
+      return res.status(400).render("users/change-password", {
+        pageTitle: "Change Password",
+        errorMessage: "The password does not match the confirmation",
+      });
+    }
+    user.password = newPassword;
+    await user.save();
+    return res.redirect("/users/logout");
+  };
+
+export const see = async(req, res) => {
+    const { id } = req.params;
+    const user = await userModel.findById(id).populate("videos");
+    if(!user) {
+        return res.status(404).render("404", {pageTitle: "User not found."});
+    }
+    return res.render("users/profile", {
+        pageTitle: `${user.name}의 Profile`,
+        user,
+        }
+    )
+}
