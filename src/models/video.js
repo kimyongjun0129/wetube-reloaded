c342f622d717b0c1edf46f80c71526d2c34a18ca
kimyongjun0129 <kimyongjun0129@naver.com>
2025-02-20 23:40:33 +0900

    add fileUrl and owner
    fileUrl : Save the path where the actual video is stored
    
    owner : A user object id for distinguishing video uploaders is stored.

--- src/models/video.js
+++ src/models/video.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-    title: {type: String, required: true, trim: true, maxLength: 80},
+    title: { type: String, required: true, trim: true, maxLength: 80 },
+    fileUrl: { type: String, required: true },
     description: {type: String, required: true, trim: true, minLength: 10},
     createdAt: {type: Date, required: true, default: Date.now},
     hashtags: [{type:String, trim: true}],
@@ -9,6 +10,7 @@ const videoSchema = new mongoose.Schema({
         views: { type: Number, default: 0, required: true},
         rating: { type: Number, default: 0, required: true},
     },
+    owner: { type: mongoose.Schema.Types.ObjectId, requried: true, ref:'User' },
 });
 
 videoSchema.static('formatHashtags', function(hashtags) {
