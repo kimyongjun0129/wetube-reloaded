e106665657a3645e55d2f8822baca7f2a4c2a0f2
kimyongjun0129 <kimyongjun0129@naver.com>
2025-02-20 23:45:36 +0900

    Update userRouter.js
    1. avatarUpload : multer middleware
    
    2. add route to change password

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,7 +1,7 @@
 import express from "express";
-import { getEdit, postEdit, startGithubLogin, finishGithubLogin } from "../controllers/userController.js";
+import { getEdit, postEdit, startGithubLogin, finishGithubLogin, getChangePassword, postChangePassword } from "../controllers/userController.js";
 import { see, logout  } from "../controllers/userController.js";
-import { protectorMiddleware, publicOnlyMiddleware, uploadFiles } from "../middlewares.js"
+import { protectorMiddleware, publicOnlyMiddleware, avatarUpload } from "../middlewares.js"
 
 const usersRouter = express.Router();
 
@@ -9,9 +9,10 @@ usersRouter.get("/logout", protectorMiddleware, logout);
 usersRouter.route("/edit")
 .all(protectorMiddleware)
 .get(getEdit)
-.post(uploadFiles.single("avatar"), postEdit)
+.post(avatarUpload.single("avatar"), postEdit)
 usersRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
 usersRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
-usersRouter.get(":id", see);
+usersRouter.get("/:id(\\d+)", see);
+usersRouter.route("/change-password").get(getChangePassword).post(postChangePassword);
 
 export default usersRouter;
