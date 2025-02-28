dd09631a1df28e117c25d4a0be0e288eb6e4277a
kimyongjun0129 <kimyongjun0129@naver.com>
2025-03-01 01:11:52 +0900

    Update videoRouter.js
    1. Change to fields function to upload 2 files

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -5,8 +5,16 @@ import { videoUpload } from "../middlewares.js";
 const videoRouter = express.Router();
 
 videoRouter.get("/:id([0-9a-f]{24})", watch);
-videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
-videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
-videoRouter.route("/upload").get(getUpload).post(videoUpload.single("video"), postUpload);
+videoRouter.route("/:id([0-9a-f]{24})/edit")
+    .get(getEdit)
+    .post(postEdit);
+videoRouter.route("/:id([0-9a-f]{24})/delete")
+    .get(deleteVideo);
+videoRouter.route("/upload")
+    .get(getUpload)
+    .post(videoUpload.fields([
+        {name:"video", maxCount: 1},
+        {name:"thumb", maxCount: 1},
+    ]), postUpload);
 
 export default videoRouter;
