bbbe7314a218c78261c13156f271318753477a96
kimyongjun0129 <kimyongjun0129@naver.com>
2025-03-07 22:07:32 +0900

    Update videoController.js
    1. Add createComment controller
    2. Add deleteComment controller

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,6 @@
 import userModel from "../models/user.js";
 import videoModel from "../models/video.js";
+import commentModel from "../models/Comments.js";
 
 export const home = async(req, res) => 
 {
@@ -19,7 +20,7 @@ export const home = async(req, res) =>
 export const watch = async (req, res) => 
 {
     const { id } = req.params;
-    const video = await videoModel.findById(id).populate("owner");
+    const video = await videoModel.findById(id).populate("owner").populate("comments");
     if(!video) {
         return res.render("404", {pageTitle:"Video not found."});
     }
@@ -138,4 +139,56 @@ export const registerView = async(req, res) => {
     video.meta.views += 1;
     await video.save();
     return res.sendStatus(200);
+}
+
+export const createComment = async (req, res) => {
+    const {
+        params : { id },
+        body : { text },
+        session : { user },
+    } = req;
+
+    const video = await videoModel.findById(id);
+    if(!video) {
+        req.flash("error", "Video not found");
+        return res.sendstatus(404);
+    }
+    const comment = await commentModel.create({
+        text,
+        owner: user._id,
+        video: id,
+    });
+    const commentOwner = await userModel.findById(user._id);
+    video.comments.push(comment._id);
+    commentOwner.comments.push(comment._id);
+    await video.save();
+    await commentOwner.save();
+    return res.status(201).json({newCommentId: comment._id});
+}
+
+export const deleteComment = async (req, res) => {
+    const { id } = req.body;
+    const { user:{_id} } = req.session;
+    const comment = await commentModel.findById(id).populate("owner").populate("video");
+    const video = await videoModel.findById(comment.video._id);
+    const user = await userModel.findById(_id);
+    if(!comment) {
+        req.flash("error", "Comment not found");
+        return res.sendStatus(404);
+    }
+    if(String(comment.owner._id) != String(_id)) {
+        return res.status(403).redirect("/");
+    }
+    try {
+        await commentModel.findByIdAndDelete(id);
+        video.comments = video.comments.filter(comment_id => comment_id != id);
+        user.comments = user.comments.filter(comment_id => comment_id != id);
+        await video.save();
+        await user.save();
+        return res.sendStatus(201);
+    }
+    catch (error) {
+        res.flash("error", "Cannot found comment");
+        return res.sendStatus(404);
+    }
 }
