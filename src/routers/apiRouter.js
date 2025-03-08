c7e97381d396a03e9aa9c132fd4cff65b0f9aa45
kimyongjun0129 <kimyongjun0129@naver.com>
2025-03-08 23:43:56 +0900

    Update apiRouter.js
    1. Add editComment route

--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { registerView, createComment, deleteComment } from "../controllers/videoController.js"
+import { registerView, createComment, deleteComment, editComment as editComment } from "../controllers/videoController.js"
 
 const apiRouter = express.Router();
 
 apiRouter.post("/videos/:id([0-9a-f]{24})/view", registerView)
 apiRouter.post("/videos/:id([0-9a-f]{24})/comment", createComment)
 apiRouter.delete("/comment", deleteComment)
+apiRouter.put("/comment", editComment)
 
 export default apiRouter;
