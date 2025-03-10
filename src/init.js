e43646703cde8cbfd1de5bf4d0beda5010c645c7
kimyongjun0129 <kimyongjun0129@naver.com>
2025-03-10 23:36:31 +0900

    Update init.js
    change port

--- src/init.js
+++ src/init.js
@@ -5,7 +5,7 @@ import "./models/user.js";
 import "./models/Comments.js";
 import app from "./server.js"
 
-const PORT = 4000;
+const PORT = 3000;
 
 const handleListening = () => console.log(`Server listenting on port http://localhost:${PORT}`);
 
